92d0f518d76b1da58d0b296a4fcf045a6548cf73
kumarlalapet <Pappu*1$Pro>
2016-07-28 23:00:51 -0700

    added rebalance listener and random message generator

--- src/main/java/com/mapr/demo/MessageConsumer.java
+++ src/main/java/com/mapr/demo/MessageConsumer.java
@@ -1,12 +1,12 @@
 package com.mapr.demo;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
@@ -24,6 +24,11 @@ public class MessageConsumer {
         //arg[0] - topic
         //arg[1] - group id
         //arg[2] - client id
+        //arg[3] - fileName
+
+        FileWriter fw = new FileWriter(args[3], true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter writer = new PrintWriter(bw);
 
         topic = args[0];
 
@@ -32,21 +37,15 @@ public class MessageConsumer {
         // Subscribe to the topic.
         List<String> topics = new ArrayList();
         topics.add(topic);
-        consumer.subscribe(topics);
+        consumer.subscribe(topics, new RebalanceListener());
 
         // Set the timeout interval for requests for unread messages.
-        long pollTimeOut = 1000;
+        long pollTimeOut = 3000;
 
         try {
             do {
                 // Request unread messages from the topic.
                 ConsumerRecords<String, String> consumerRecords = consumer.poll(pollTimeOut);
-
-                /**
-                 if (processRecords(consumerRecords)) {
-                 consumer.commitSync();
-                 }
-                 **/
 
                 Iterator<ConsumerRecord<String, String>> iterator = consumerRecords.iterator();
                 if (iterator.hasNext()) {
@@ -54,11 +53,21 @@ public class MessageConsumer {
                         ConsumerRecord<String, String> record = iterator.next();
 
                         if (processRecords(record)) {
-                            System.out.println((" Consumed Record: " + record.toString()));
-
                             Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
-                            offsets.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset()));
-                            consumer.commitSync(offsets);
+                            offsets.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset()+1));
+                            consumer.commitAsync(offsets, new OffsetCommitCallback() {
+                                @Override
+                                public void onComplete(Map<TopicPartition, OffsetAndMetadata> map, Exception e) {
+                                    //Any after commit work can be done here
+                                }
+                            });
+
+                            String message = record.toString();
+                            String value = record.value();
+                            System.out.println((" Consumed Record: " + message));
+                            writer.println(value);
+                            writer.flush();
+
                         }
 
                     }
@@ -67,24 +76,34 @@ public class MessageConsumer {
             } while (true);
         } finally {
             consumer.close();
+            writer.close();
         }
-    }
-
-    private static boolean processRecords(ConsumerRecords<String, String> consumerRecords) {
-        Iterator<ConsumerRecord<String, String>> iterator = consumerRecords.iterator();
-        if (iterator.hasNext()) {
-            while (iterator.hasNext()) {
-                ConsumerRecord<String, String> record = iterator.next();
-                System.out.println((" Consumed Record: " + record.toString()));
-            }
-        }
-
-        return true;
     }
 
     private static boolean processRecords(ConsumerRecord<String, String> record) {
-        //Done processing record
         return true;
+    }
+
+    static class RebalanceListener implements ConsumerRebalanceListener {
+
+        public RebalanceListener() {
+        }
+
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
+            for(TopicPartition partition: partitions) {
+                offsets.put(partition, new OffsetAndMetadata(consumer.position(partition)) );
+            }
+            consumer.commitSync(offsets);
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            for(TopicPartition partition: partitions) {
+                consumer.seek(partition, consumer.position(partition));
+            }
+        }
     }
 
     /* Set the value for a configuration parameter.
